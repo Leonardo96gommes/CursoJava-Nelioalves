0c08e53b3bfac14986762f890c5ff3d710d27269
Leonardo Gomes <117546818+Leonardo96gommes@users.noreply.github.com>
2025-02-18 22:47:21 -0300

    Restrições e variaveis

---
+++ Java/src/Restriçoes_Variaveis.java
@@ -0,0 +1,32 @@
+public class Restriçoes_Variaveis {
+    public static void main(String[] args) {
+        // Restrições para nomes de variáveis
+        //• Não pode começar com dígito: use uma letra ou _
+        //• Não usar acentos ou til
+        //• Não pode ter espaço em branco
+        //• Sugestão: use nomes que tenham um significado
+
+        Errado:
+        int 5minutes;
+        int salário;
+        int salario do funcionario;
+
+        Correto:
+        int _5minutes;
+        int salario;
+        int salarioDoFuncionario;
+
+
+        //Convenções
+        //• Camel Case: lastName
+        //• pacotes
+        //• atributos
+        //• métodos
+        //• variáveis e parâmetros
+        //• Pascal Case: ProductService
+        //• classes
+
+
+    }
+
+}
