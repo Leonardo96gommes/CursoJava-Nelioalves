1b27a8114a76c79079d9767b22b78f3de60f3ead
Leonardo Gomes <117546818+Leonardo96gommes@users.noreply.github.com>
2025-02-21 22:17:22 -0300

    atualização programa

--- Java/src/entities/Triangulo.java
+++ Java/src/entities/Triangulo.java
@@ -1,9 +1,13 @@
 package entities;
 
 public class Triangulo {
-    public  double a;
-    public  double b;
-    public  double c;
+    public double a; // Lado 'a' do triângulo
+    public double b; // Lado 'b' do triângulo
+    public double c; // Lado 'c' do triângulo
 
+    // Método para calcular a área do triângulo usando a fórmula de Heron
+    public double area() {
+        double p = (a + b + c) / 2.0; // Calcula o semiperímetro do triângulo
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c)); // Calcula e retorna a área do triângulo
+    }
 }
-
--- Java/src/program/program.java
+++ Java/src/program/program.java
@@ -5,7 +5,7 @@ import entities.Triangulo;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class exe26 {
+public class program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US); // Define o local padrão para os EUA, garantindo o uso do ponto como separador decimal
         Scanner sc = new Scanner(System.in); // Cria um objeto Scanner para ler a entrada do usuário
@@ -22,11 +22,8 @@ public class exe26 {
         y.b = sc.nextDouble(); // Lê a segunda medida do triângulo Y
         y.c = sc.nextDouble(); // Lê a terceira medida do triângulo Y
 
-        double p = (x.a + x.b + x.c) / 2.0; // Calcula o semiperímetro do triângulo X
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c)); // Calcula a área do triângulo X usando a fórmula de Heron
-
-        p = (y.a + y.b + y.c) / 2.0; // Calcula o semiperímetro do triângulo Y
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c)); // Calcula a área do triângulo Y usando a fórmula de Heron
+        double areaX = x.area(); // Calcula a área do triângulo X
+        double areaY = y.area(); // Calcula a área do triângulo Y
 
         System.out.printf("Triangle X area: %.4f%n", areaX); // Imprime a área do triângulo X com 4 casas decimais
         System.out.printf("Triangle Y area: %.4f%n", areaY); // Imprime a área do triângulo Y com 4 casas decimais
@@ -36,7 +33,6 @@ public class exe26 {
         } else {
             System.out.println("Larger area: Y"); // Imprime que a área do triângulo Y é maior
         }
-
         sc.close(); // Fecha o objeto Scanner para liberar recursos
     }
 }
